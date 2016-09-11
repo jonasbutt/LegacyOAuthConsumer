d0530be1c0c31f779cde407281643e68e72a99ac
jonasbutt <jonas.butt@gmail.com>
2016-09-11 19:49:59 +0200

    Solution setup.

---
+++ Source/LegacyOAuthConsumer.Test/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace LegacyOAuthConsumer.Test
+{
+    public class PlaceholderTestClass
+    {
+        [Fact]
+        public void PlaceholderTest() 
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Source/LegacyOAuthConsumer/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LegacyOAuthConsumer")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("07389eb2-19bf-4007-8221-0a9f8ef7efb7")]
